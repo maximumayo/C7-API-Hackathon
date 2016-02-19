742bf9da2c565b1aa517a84cc6e17f7e6c8cf361
Jaison Amos <jaisonamos@gmail.com>
2016-02-18 17:40:24 -0800

    worked on flickr api, have one concatenated string url example

---
+++ script.js
@@ -0,0 +1,25 @@
+/**
+ * Created by jaisonamos on 2/18/16.
+ */
+var global_result;
+$(document).ready(function() {
+    $.ajax({
+        datatype: 'json',
+        method: "get",
+        url: 'https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=6cf32b7431855ee07e7a0749b21399b2&format=json&nojsoncallback=1&text=newyork',
+        success: function (result) {
+            console.log('ajax was a success' + result);
+
+            global_result = result;
+
+            var farm = global_result.photos.photo[0].farm;
+            var id = global_result.photos.photo[0].id;
+            var secret = global_result.photos.photo[0].secret;
+            var server = global_result.photos.photo[0].server;
+            var url = ('https://farm' + farm +'.staticflickr.com/' + server + "/" + id + "_" + secret + '.jpg' );
+            console.log(url);
+
+        }
+    });
+
+});
