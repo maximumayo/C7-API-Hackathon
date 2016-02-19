9aef651a531bfd2489eb2afcd4df675c92bc7cc1
Trisha Meyer <tmeye62@gmail.com>
2016-02-19 14:10:48 -0800

    issue #7 edited comment on background slideshow

--- backgroundSlide.js
+++ backgroundSlide.js
@@ -1,7 +1,7 @@
 /**
  * Created by trishameyer on 2/19/16.
  */
-//Array of images which you want to show: Use path you want.
+//Array of images which you want to show:
 var images = ['http://hdwallpaperbackgrounds.net/wp-content/uploads/2015/08/Redwood-National-Park-California-Wallpapers-2560x1600.jpg','http://backgroundimagesfree.com/wp-content/uploads/2014/12/brooklyn_bridge_nyc-Wallpaper-HD-Desktop-Background.jpg','http://host2post.com/server13/bgs/l6/l61cbtromry80m~.jpg', 'https://wdpromedia.disney.go.com/media/campaigns/2/media/grandadventure/images/Offer-background.jpg', 'https://www.azamaraclubcruises.com/sites/default/files/heros/key-west-florida.jpg'];
 
 var nextimage=0;
