da34b028ec56d09ff125fc8c65608cfc8600387c
Deanna Sulli <dtcsulli@gmail.com>
2016-02-19 11:56:34 -0800

    modal title and tabs change based on what was selected in the dropdowns

--- click.js
+++ click.js
@@ -10,7 +10,22 @@ $(document).ready(function () {
         var value = $(this).text();
         $('#interestMenu .dropdown-button-text').text(value);
     });
+
     $('.submit').on('click', function () {
+
+        var stateName = $('#desinationMenu .dropdown-button-text').text();
+        var interestName = $('#interestMenu .dropdown-button-text').text();
+
+        //changes the title of the modal to the state that was selected
+        $('.modal-title').text(stateName);
+
+        //changes the tab in the modal to the interest that was selected
+        $('.active').removeClass('active'); //removes active class on default active tab
+        var interestDiv = interestName.toLowerCase() + 'Div'; //creates a string of the id name based on the value of the interest dropdown menu
+        $('#'+interestDiv).addClass('active'); //adds class active to the div that holds the interest's content
+        $('a[href="#'+ interestDiv +'"]').parent().addClass('active'); //adds class active to the li of the interest's tab
+
+
         $('#interestModal').on('shown.bs.modal', function () { //Wait event for modal to show before adding google maps
             disney.postMap();
         });
